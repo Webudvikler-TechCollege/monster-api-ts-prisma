2f977fb1a91c1aecd68fd0c163623ea97be16409
Heinz K <heka@techcollege.dk>
2025-08-06 13:18:34 +0200

    Added image functionality

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,21 @@ import { userRoutes } from './routes/userRoutes';
 import { productRoutes } from './routes/productRoutes';
 import { authRoutes } from './routes/authRoutes';
 import { favoriteRoutes } from './routes/favoriteRoutes';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+// Gør public-mappen tilgængelig som statisk
+app.use('/assets', express.static(path.join(__dirname, '../public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
